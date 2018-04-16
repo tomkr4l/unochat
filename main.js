c8ef70a59f75218da5e2b6e1a0d0d56a7351fb40
Tomáš Král <kralutomas@gmail.com>
2018-04-16 21:44:09 +0200

    readme

--- main.js
+++ main.js
@@ -13,7 +13,13 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600, icon: 'assets/images/icon.png'})
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: 'assets/images/icon.png',
+    minWidth: 300,
+    minHeight: 400
+    })
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
